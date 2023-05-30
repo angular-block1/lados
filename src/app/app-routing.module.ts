5cc8fb66ca41d2773e85a3f2f47e29479d862cc6
Duong Van Toan <toandvph19458@fpt.edu.vn>
2023-05-30 20:48:54 +0700

    WQXF-6 routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutClientComponent } from './layouts/layout-client/layout-client.component';
+import { ListProductComponent } from './pages/list-product/list-product.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { CartComponent } from './pages/cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:LayoutClientComponent,children:[
+    {path:"product", component:ListProductComponent},
+    {path:"product/:id", component:ProductDetailComponent},
+    {path:"cart", component:CartComponent},
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
