db437ead5ac0da1d145874426d2ba8730c9b3eda
TruongNguyen <truongmoba157@gmail.com>
2023-06-03 09:43:53 +0700

    feat/WQXF-12: Trang checkout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './component/header/header.component';
-import { FooterComponent } from './component/footer/footer.component';
-import { LayoutAdminComponent } from './layouts/layout-admin/layout-admin.component';
-import { LayoutClientComponent } from './layouts/layout-client/layout-client.component';
-import { FormsModule } from '@angular/forms';
-import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
-import { CartComponent } from './pages/cart/cart.component';
-import { ListProductComponent } from './pages/list-product/list-product.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-<<<<<<< Updated upstream
-import { LoginComponent } from './pages/login/login.component';
-=======
-import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
->>>>>>> Stashed changes
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./component/header/header.component";
+import { FooterComponent } from "./component/footer/footer.component";
+import { LayoutAdminComponent } from "./layouts/layout-admin/layout-admin.component";
+import { LayoutClientComponent } from "./layouts/layout-client/layout-client.component";
+import { FormsModule } from "@angular/forms";
+import { ProductDetailComponent } from "./pages/product-detail/product-detail.component";
+import { CartComponent } from "./pages/cart/cart.component";
+import { ListProductComponent } from "./pages/list-product/list-product.component";
+import { HomePageComponent } from "./pages/home-page/home-page.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { SignUpPageComponent } from "./pages/sign-up-page/sign-up-page.component";
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    LayoutAdminComponent,
-    LayoutClientComponent,
-    ProductDetailComponent,
-    CartComponent,
-    ListProductComponent,
-    HomePageComponent,
-<<<<<<< Updated upstream
-    LoginComponent
-=======
-    SignUpPageComponent
->>>>>>> Stashed changes
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, HeaderComponent, FooterComponent, LayoutAdminComponent, LayoutClientComponent, ProductDetailComponent, CartComponent, ListProductComponent, HomePageComponent, LoginComponent, SignUpPageComponent],
+	imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { faUser, faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+	selector: "app-header",
+	templateUrl: "./header.component.html",
+	styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent {
-
+	faUser = faUser;
+	faCartShopping = faCartShopping;
 }
