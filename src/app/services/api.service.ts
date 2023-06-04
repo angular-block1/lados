966314a9ff7d46ba0c3b2533067faa4c0034653c
TruongNguyen <truongmoba157@gmail.com>
2023-06-04 22:14:25 +0700

    feat/WQXF-19: cấu hình service

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable({
+	providedIn: "root",
+})
+export class ApiService {
+	private baseUrl = "http://localhost:3000";
+	constructor(private _http: HttpClient) {}
+
+	getTypeRequest(url: string): Observable<any> {
+		return this._http.get(`${this.baseUrl}${url}`);
+	}
+
+	postTypeRequest(url: string, payload: any): Observable<any> {
+		return this._http.post(`${this.baseUrl}${url}`, payload);
+	}
+
+	deleteTypeRequest(url: string): Observable<any> {
+		return this._http.delete(`${this.baseUrl}${url}`);
+	}
+
+	putTypeRequest(url: string, payload: any): Observable<any> {
+		return this._http.put(`${this.baseUrl}${url}`, payload);
+	}
+}
