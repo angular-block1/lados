6827f7ec4aee0d4358952104afc457c7fa5dd1fa
TruongNguyen <truongmoba157@gmail.com>
2023-06-06 23:12:05 +0700

    feat: quản trị danh mục refs WQXF-25

---
+++ src/app/services/category.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { ApiService } from "./api.service";
+
+@Injectable({
+	providedIn: "root",
+})
+export class CategoryService {
+	constructor(private _http: HttpClient, private _api: ApiService) {}
+
+	getCategories(): Observable<any> {
+		return this._api.getTypeRequest("/categories");
+	}
+
+	getCategory(slug: string): Observable<any> {
+		return this._api.getTypeRequest(`/categories?slug=${slug}`);
+	}
+
+	createCategory(payload: any): Observable<any> {
+		return this._api.postTypeRequest("/categories", payload);
+	}
+
+	updateCategory(id: string, payload: any): Observable<any> {
+		return this._api.putTypeRequest(`/categories/${id}`, payload);
+	}
+
+	deleteCategory(id: string): Observable<any> {
+		return this._api.deleteTypeRequest(`/categories/${id}`);
+	}
+}
