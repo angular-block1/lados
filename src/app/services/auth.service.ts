d5d83b356f514e5af5c25f9a3ec0d0d65134a00c
toandvph19458 <105204332+toandvph19458@users.noreply.github.com>
2023-06-09 16:12:56 +0700

    feat: đăng nhập refs WQXF-35-chức-nang-dang-nhập (#32)
    Co-authored-by: Duong Van Toan <toandvph19458@fpt.edu.vn>

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs';
+import { Iauth } from 'app/interfaces/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private _http: HttpClient, private _api: ApiService) { }
+  
+  sigin(payload: any): Observable<Iauth> {
+    return this._api.postTypeRequest("/auth/signin", payload);
+  }
+  signup(payload: any): Observable<any> {
+    return this._api.postTypeRequest("/auth/signin", payload);
+  }
+}
